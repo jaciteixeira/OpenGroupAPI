7ed2df54c494782d5d6715f43c2a6445ac8dfa8c
jaciteixeira <jaci9306@gmail.com>
2024-04-13 19:18:06 -0300

    removido api-key

--- src/main/java/br/com/fiap/opengroup/service/PredicaoService.java
+++ src/main/java/br/com/fiap/opengroup/service/PredicaoService.java
@@ -12,8 +12,8 @@ import java.util.List;
 @Service
 public class PredicaoService {
 
-    @Value("${openai.api.key}")
-    private String API_KEY;
+//    @Value("${openai.api.key}")
+    private String API_KEY = "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";
 
 
     public RestTemplate restTemplate(){
